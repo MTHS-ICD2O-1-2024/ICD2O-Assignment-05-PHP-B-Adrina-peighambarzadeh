b1f46acba1b19d8b3dedf9c51395de2da4eb4616
Adrina-peighambarzadeh <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-26 18:28:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
+
 <!DOCTYPE html>
 <html>
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Factorial Calculator in PHP" />
-  <meta name="keywords" content="math, factorial, icd2o" />
-  <meta name="author" content="Adrina Peighambarzadeh" />
+  <meta name="description" content="Exponents, with PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Adrina peighambarzadeh" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-indigo.min.css" />
-  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-red.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="site.webmanifest" />
-  <title>Factorial Calculator in PHP</title>
+  <title>Factorial Calculator with PHP</title>
 </head>
 
 <body>
@@ -22,30 +23,31 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Factorial Calculator in PHP</span>
+        <span class="mdl-layout-title">Factorial Calculator with PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/image.png" alt="Factorial Icon" width="250" />
+        <img src="./images/image.png" alt="Factorial image" width="500" />
       </div>
       <br />
-      <div class="page-content">Enter a non-negative integer to calculate its factorial:</div>
-      <div class="page-content-php">
-        <form action="answer.php" method="GET">
-          <br />
-          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-            <input class="mdl-textfield__input" type="text" id="user-number" name="user-number">
-            <label class="mdl-textfield__label" for="user-number">Enter a number</label>
-          </div>
-          <br />
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-            type="submit">
-            Calculate
-          </button>
-        </form>
+      <div class="page-content-guide">Enter a non-negative number to calculate the factorial:</div>
+      <div class="page-content">
+        <form action="./answer.php" method="GET">
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="base-number" />
+            <label class="mdl-textfield__label" for="base-number">Enter number here...</label>
+            <span class="mdl-textfield__error">Please enter a non-negative integer!</span>
       </div>
-    </main>
+      <br />
+      <!-- Accent-colored raised button with ripple -->
+      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+        type="submit">
+        Calculate
+      </button>
+      </form>
+  </div>
+  </main>
   </div>
 </body>
 
