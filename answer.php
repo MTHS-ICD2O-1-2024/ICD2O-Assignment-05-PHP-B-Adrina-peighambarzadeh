770ddfc7bdc99d0dfa160b760255a4eb2bc1563d
Adrina-peighambarzadeh <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-26 18:34:16 -0400

    Update answer.php

--- answer.php
+++ answer.php
@@ -3,64 +3,62 @@
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Factorial Calculation in PHP" />
-  <meta name="keywords" content="math, factorial, icd2o" />
-  <meta name="author" content="Adrina Peighambarzadeh" />
+  <meta name="description" content="Factorial Calculator, with PHP" />
+  <meta name="keywords" content="math, factorial, php, icd2o" />
+  <meta name="author" content="Adrina peighambarzadeh" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-light_green.min.css" />
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible+Mono&display=swap" rel="stylesheet">
-  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
-  <link rel="manifest" href="/site.webmanifest">
-  <link rel="stylesheet" href="css/style.css">
-  <title>Factorial Result</title>
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-red.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Factorial Calculator, with PHP</title>
 </head>
 
 <body>
-  <div class="container-2">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">Factorial Result</span>
-        </div>
-      </header>
-      <main class="mdl-layout__content">
-        <div class="right-image">
-        </div>
-        <br><br>
-        <?php
-        // Get the input number
-        $inputNumber = $_GET["input-number"];
-        $factorial = 1;
-        $counter = 1;
-        $calculationSteps = "";
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Factorial Calculator, with PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/image.png"" alt="Factorial image" width="500" />
+      </div>
+      <div class="page-content-answer">
+        <div id="answer">
+          <?php
+            // input
+            $factorial = 1;
 
-        echo "<b>Input Number: " . $inputNumber . "</b><br><br>";
+            if (isset($_GET["number"])) {
+              $number = $_GET["number"];
 
-          // Calculate factorial with loop
-          while ($counter <= $inputNumber) {
-            $factorial *= $counter;
-            $calculationSteps .= $counter;
-            if ($counter < $inputNumber) {
-              $calculationSteps .= " × ";
+              // validation: check if it's a non-negative whole number
+              if ($number >= 0 && floor($number) == $number) {
+                // process using counter
+                $counter = 1;
+                while ($counter <= $number) {
+                  $factorial = $factorial * $counter;
+                  $counter = $counter + 1;
+                }
+
+                // output
+                echo "The factorial of " . $number . " is: " . $factorial . ".";
+              } else {
+                echo "Please enter a valid non-negative whole number.";
             }
-            $counter++;
-          }
-
-          echo "<b>Calculation: " . $calculationSteps . " = " . $factorial . "</b>";
-
-        ?>
-        <br><br>
-        <div class="page-content-answer">
-          <a href="./index.php">Return to Factorial Calculator</a>
+          ?>
         </div>
-        <br>
-      </main>
-    </div>
+      </div>
+      <div class="page-content-return">
+        <a href="./index.php">Return ...</a>
+      </div>
+    </main>
   </div>
 </body>
 
